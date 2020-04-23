d472657f51b6cfc4dfc21803f699fd64b2f22d27
ken1286 <ken.ridenour4@gmail.com>
2020-04-23 07:57:10 -0700

    Register component cleaned up with error messages

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,7 +26,7 @@ const Login = (props) => {
         history.push('/');
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err.response);
       });
   };
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,107 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-class Register extends Component {
-  state = {
-    username: '',
-    password1: '',
-    password2: '',
-  };
+const Register = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState(null);
+  const history = useHistory();
 
-  inputChangeHandler = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  submitValues = ({ username, password1, password2 }) => {
-    console.log({ username, password1, password2 });
-    this.setState({
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const credentials = {
       username: username,
-      password1: password1,
-      password2: password2,
-    });
-    const herokurl = '';
-    console.log('this state', this.state);
+      password: password,
+    };
+    const testURL = 'http://localhost:5000/api/';
 
     axios({
-      url: `${herokurl}/api/registration/`,
+      url: `${testURL}auth/register`,
       method: 'POST',
-      data: {
-        username: `${this.state.username}`,
-        password1: `${this.state.password1}`,
-        password2: `${this.state.password2}`,
-      },
+      data: credentials,
     })
       .then((res) => {
-        console.log('response', res);
-        const token = res.data['key'];
-        localStorage.setItem('token', `${token}`);
-        this.props.history.push('/');
+        localStorage.setItem('token', res.data.token);
+        console.log(res);
+        history.push('/');
       })
       .catch((err) => {
-        console.log('Axios error:', err.response);
-        console.log('this state', this.state);
-        if (err.response.data.password1 && !err.response.data.email) {
-          this.setState({ errorMsg: [...err.response.data.password1] });
-        } else if (err.response.data.email && !err.response.data.password1) {
-          this.setState({ errorMsg: [...err.response.data.email] });
-        } else {
-          this.setState({
-            errorMsg: [...err.response.data.password1],
-          });
+        const errResponse = err.response.data;
+        console.log(errResponse);
+        if (errResponse.username && errResponse.password) {
+          setErrorMsg([errResponse.username, errResponse.password]);
+        } else if (errResponse.username && !errResponse.password) {
+          setErrorMsg([errResponse.username]);
+        } else if (errResponse.password && !errResponse.username) {
+          setErrorMsg([errResponse.password]);
         }
-
-        console.log(this.state);
       });
   };
 
-  submitHandler = (ev) => {
-    ev.preventDefault();
-    this.submitValues(this.state);
-  };
-
-  render() {
-    return (
-      <div className='register-div'>
-        <form onSubmit={(e) => this.submitHandler(e)}>
-          <h1>Register</h1>
-          <input
-            placeholder='Username'
-            type='text'
-            name='username'
-            value={this.state.username}
-            onChange={this.inputChangeHandler}
-          ></input>
-          <input
-            placeholder='Password'
-            type='password'
-            name='password1'
-            value={this.state.password1}
-            onChange={this.inputChangeHandler}
-          ></input>
-          <input
-            placeholder='Confirm password'
-            type='password'
-            name='password2'
-            value={this.state.password2}
-            onChange={this.inputChangeHandler}
-          ></input>
-          {this.state.errorMsg
-            ? this.state.errorMsg.map((item) => {
-                return <p style={{ color: 'red', fontSize: '12px' }}>{item}</p>;
-              })
-            : null}
-          <p>
-            <input
-              type='submit'
-              value='Register'
-              onSubmit={(e) => this.submitHandler(e)}
-            ></input>
-          </p>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='register-div'>
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        <input
+          placeholder='Username'
+          type='text'
+          name='username'
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+            console.log(username);
+          }}
+        ></input>
+        <input
+          placeholder='Password'
+          type='password'
+          name='password'
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            console.log(password);
+          }}
+        ></input>
+        {errorMsg
+          ? errorMsg.map((item) => {
+              return <p style={{ color: 'red', fontSize: '12px' }}>{item}</p>;
+            })
+          : null}
+        <p>
+          <input type='submit' value='Register' onSubmit={handleSubmit}></input>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
