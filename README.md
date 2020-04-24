c6c5502898e188ae423cfc0fc7379ff7e07188e4
ken1286 <ken.ridenour4@gmail.com>
2020-04-24 07:57:00 -0700

    updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1. `npm install` to install all required dependencies.
 
 2. `npm run start` will run the project in your browser.
+
+Login and Register work to create a new user
+username: `user1` password: `12345` is a mock user with some premade todos.
--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -65,7 +65,7 @@ const AddTodoForm = (props) => {
             placeholder='...add todo'
             value={todoText}
             onChange={(e) => setTodoText(e.target.value)}
-            maxLength='100'
+            maxLength='50'
             style={{ width: 600 }}
           />
 
